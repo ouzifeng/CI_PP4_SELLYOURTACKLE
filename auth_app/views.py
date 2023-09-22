c665735bab59ffb586852df325a21b14105338cd
David Oak <mr.davidoak@gmail.com>
2023-09-22 16:57:47 +0100

    start on signup form

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserSignupForm
 
 def signup(request):
     return render(request, 'signup.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = CustomUserSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Here you can also log the user in or send a confirmation email if needed
+            return redirect('/')  # Redirect to a success page or login page
+    else:
+        form = CustomUserSignupForm()
+    return render(request, 'auth_app/signup.html', {'form': form})
