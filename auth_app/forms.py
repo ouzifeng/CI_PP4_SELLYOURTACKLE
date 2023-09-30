91cad15c2b00bf06a0badd0a335fbfd20de54fdc
David Oak <mr.davidoak@gmail.com>
2023-09-30 20:21:03 +0100

    add client and server side validation to signup form

--- auth_app/forms.py
+++ auth_app/forms.py
@@ -1,37 +1,77 @@
 from django import forms
 from .models import CustomUser
 from crispy_forms.helper import FormHelper
+from django.forms.widgets import EmailInput
+from django.core.exceptions import ValidationError
+
 
 class CustomUserSignupForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    # Sign up form for new users
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'required': True, 
+            'pattern': "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", 
+            'title': "Please enter a valid email address"
+        })
+    )
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'required': True,
+            'minlength': 3,
+            'title': "First name must be at least 3 characters long."
+        })
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'required': True,
+            'minlength': 3,
+            'title': "Last name must be at least 3 characters long."
+        })
+    )
+
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={
+            'required': True,
+            'minlength': 8,
+            'title': "Password must be at least 8 characters long, include a mix of letters and numbers."
+        })
+    )
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(attrs={
+            'required': True,
+            'minlength': 8,
+            'title': "Please re-enter the password."
+        })
+    )
 
     class Meta:
         model = CustomUser
         fields = ['email', 'first_name', 'last_name']
 
     def clean_email(self):
-        # Validate unique email
         email = self.cleaned_data.get('email')
         if CustomUser.objects.filter(email=email).exists():
             raise forms.ValidationError("Email already exists")
         return email
 
     def clean_password2(self):
-        # Check that the two password entries match
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
+
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
-        return password2
+        
+        if len(password1) < 8:
+            raise ValidationError("Password must be at least 8 characters long.")
+        if not any(char.isdigit() for char in password1):
+            raise ValidationError("Password must contain at least one number.")
+        if not any(char.isalpha() for char in password1):
+            raise ValidationError("Password must contain at least one letter.")
 
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
+        return password2
 
     def __init__(self, *args, **kwargs):
         super(CustomUserSignupForm, self).__init__(*args, **kwargs)
