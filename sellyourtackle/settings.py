cb50e9cde64237922d1410bafbe1f0d87a978e9f
David Oak <mr.davidoak@gmail.com>
2023-10-07 19:46:25 +0100

    move server to neon postgres

--- sellyourtackle/settings.py
+++ sellyourtackle/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,10 +111,16 @@ WSGI_APPLICATION = "sellyourtackle.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sellyourtackle',
+        'USER': 'ouzifeng',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': 'ep-shiny-flower-350995.eu-central-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
@@ -177,7 +184,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 
-load_dotenv() 
+
 MAILJET_API_KEY = os.getenv('MAILJET_API_KEY')
 MAILJET_SECRET_KEY = os.getenv('MAILJET_SECRET_KEY')
 
