e06464be7887343bae9b0b50b5952e023342ee23
David Oak <mr.davidoak@gmail.com>
2023-10-09 15:14:00 +0100

    orders updating when user exists. Add pritn statements for if payment fails for degugging

--- auth_app/stripe.py
+++ auth_app/stripe.py
@@ -91,16 +91,64 @@ def stripe_webhook(request):
 
     elif event.type == 'payment_intent.payment_failed':
         payment_intent = event.data.object
-        related_session = stripe.checkout.Session.list(payment_intent=payment_intent.id)[0]
-        client_reference_id = related_session.client_reference_id
+        print("Webhook triggered for payment_intent.payment_failed")
+        print(f"Payment intent ID: {payment_intent.id}")
 
         try:
-            order = Order.objects.get(id=client_reference_id)
-            order.payment_status = 'failed'
-            order.save()
+            # Fetch the related session using the payment intent
+            print("Fetching related session for the payment intent")
+            related_session = stripe.checkout.Session.list(payment_intent=payment_intent.id)[0]
+            client_reference_id = related_session.client_reference_id
+            print(f"Client reference ID from related session: {client_reference_id}")
 
-        except Order.DoesNotExist:
+            # Extract email and name from the related session
+            email = related_session.customer_details.email
+            full_name = related_session.customer_details.name
+            first_name, *middle_names, last_name = full_name.split()
+            first_name = " ".join([first_name] + middle_names)
+
+            print(f"Email from related session: {email}")
+
+            # Get or create user
+            print("Attempting to get or create user")
+            user, created = CustomUser.objects.get_or_create(
+                email=email,
+                defaults={
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'is_active': False,
+                    'is_staff': False
+                }
+            )
+
+            if created:
+                print(f"User with email {email} created")
+            else:
+                print(f"User with email {email} already exists")
+
+            # Fetch the order and update its status
+            print("Attempting to fetch order")
+            order = Order.objects.get(id=client_reference_id)
+            print(f"Fetched order with ID {client_reference_id}")
+
+            # Attach the user to the order
+            print("Associating user with the order")
+            order.user = user
+
+            order.payment_status = 'failed'
+            print("Updating order's payment status to 'failed'")
+            order.save()
+            print("Order saved successfully")
+
+        except CustomUser.DoesNotExist:
+            print(f"No user found with email: {email}")
             return JsonResponse({'status': 'error'}, status=400)
+        except Order.DoesNotExist:
+            print(f"Order with ID {client_reference_id} does not exist")
+            return JsonResponse({'status': 'error'}, status=400)
+        except Exception as e:
+            print(f"Unexpected error occurred: {str(e)}")
+            return JsonResponse({'status': 'error'}, status=500)
 
     return JsonResponse({'status': 'success'})
 
