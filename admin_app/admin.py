f32ef948da8a93b88388fae7effde5985c8759b1
David Oak <mr.davidoak@gmail.com>
2023-10-17 12:13:40 +0100

    when order status is changed to refunded in admin panel it refunds in stripe

--- admin_app/admin.py
+++ admin_app/admin.py
@@ -1,28 +1,26 @@
 from django.contrib import admin, messages
 from tackle.models import Product
 from auth_app.models import CustomUser, Order
+import stripe
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'product_cost', 'status', 'payment_status', 'created_at')
     list_filter = ('status', 'payment_status', 'created_at')
     search_fields = ('user__email', 'status', 'payment_intent_id')
-    
-    actions = ['make_refunded']
 
-    def make_refunded(self, request, queryset):
-        # Loop through the selected orders in the admin
-        for order in queryset:
+    def save_model(self, request, obj, form, change):
+        # If the order's status has been changed to "refunded"
+        if 'status' in form.changed_data and obj.status == 'refunded':
             # Refund the order using Stripe's API
             try:
-                stripe.Refund.create(payment_intent=order.payment_intent_id)
-                order.status = 'refunded'
-                order.payment_status = 'refunded'
-                order.save()
-                self.message_user(request, f"Order {order.id} has been marked as refunded.")
+                stripe.Refund.create(payment_intent=obj.payment_intent_id)
+                obj.payment_status = 'refunded'
+                messages.success(request, f"Order {obj.id} has been marked as refunded.")
             except stripe.error.StripeError as e:
-                self.message_user(request, f"Stripe error for Order {order.id}: {str(e)}", level=messages.ERROR)
+                messages.error(request, f"Stripe error for Order {obj.id}: {str(e)}")
+                return  # Do not save the model if refund failed
+        super().save_model(request, obj, form, change)
 
-    make_refunded.short_description = "Mark selected orders as Refunded"
 
 admin.site.register(Order, OrderAdmin)
 
