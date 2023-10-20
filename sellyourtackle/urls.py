ad9273a15a0306964375dfb39cc48517691e462f
David Oak <mr.davidoak@gmail.com>
2023-10-20 12:42:32 +0100

    add apple domain verification file for Apple pay to work on Checkout

--- sellyourtackle/urls.py
+++ sellyourtackle/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+import os
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls')),
     path('', include('tackle.urls')),
     path('accounts/', include('allauth.urls')),
+    re_path(r'^\.well-known/apple-developer-merchantid-domain-association$', serve, {
+        'document_root': os.path.join(settings.BASE_DIR, 'verification-files', '.well-known'),
+        'path': 'apple-developer-merchantid-domain-association'
+    }),
 ]
