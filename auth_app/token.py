2f3a3b18783a532d0fd4c64dc56ea1ecab193079
David Oak <mr.davidoak@gmail.com>
2024-01-15 10:13:37 +0000

    fix tackle app urls.py sp they conform to pep8 standards

--- auth_app/token.py
+++ auth_app/token.py
@@ -1,15 +1,19 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils import six
 
+
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     """
     Generates random tkon for used for verifying a password
     reset
     """
+
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk) + six.text_type(timestamp) +
-            six.text_type(user.is_active)
+            six.text_type(user.pk)
+            + six.text_type(timestamp)
+            + six.text_type(user.is_active)
         )
+
 
 account_activation_token = AccountActivationTokenGenerator()
--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,72 +1,52 @@
 from django.urls import path
-
-# Authentication views
+from . import views
 from .views import (
-    SignupView,
-    CustomLoginView,
-    ConfirmEmailView,
-    LogoutView,
-    ConfirmEmailPageView,
-    AboutUsView,
-    ContactUsView,
-    PrivacyView,
-    TermsView,
-    ResetPasswordView,
-    ResetPasswordConfirmView
+    ListProduct,
+    EditProduct,
+    ProductPage,
+    HomeView,
+    AddToCartView,
+    CartView,
+    RemoveFromCartView,
+    CheckoutSuccessView,
+    CheckoutView,
+    ProductSoldView,
+    OrderPageView,
+    OrderConfirmation,
+    SearchView,
+    ShopView,
 )
-
-# User actions/views
-from .views import Buying, Selling, WalletView
-
-# Stripe related views
-from .stripe import (
-    stripe_webhook,
-    handle_payment,
-    create_stripe_express_account,
-    create_stripe_account_link,
-    handle_stripe_return
-)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='signup'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path("", HomeView.as_view(), name="home"),
+    path("list-product/", ListProduct.as_view(), name="list-product"),
+    path("search_brands/", views.SearchBrands.as_view(), name="search_brands"),
     path(
-        'confirm-email-link/', ConfirmEmailPageView.as_view(),
-        name='confirm-email-link'
-        ),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path(
-        'confirm-email/<int:user_id>/<str:token>/',
-        ConfirmEmailView.as_view(), name='confirm-email'
+        "search_categories/", views.SearchCategories.as_view(), name="search_categories"
     ),
-    path('buying/', Buying.as_view(), name='buying'),
-    path('selling/', Selling.as_view(), name='selling'),
-    path('stripe_webhook/', stripe_webhook, name='stripe_webhook'),
-    path('handle_payment/', handle_payment, name='handle_payment'),
-    path('wallet/', WalletView.as_view(), name='wallet'),
+    path("edit-product/<int:product_id>/", EditProduct.as_view(), name="edit_product"),
+    path("product/<slug:slug>", ProductPage.as_view(), name="product"),
     path(
-        'create_stripe_express_account/', create_stripe_express_account,
-        name='create_stripe_express_account'
-        ),
+        "delete-product/<int:product_id>/", views.delete_product, name="delete_product"
+    ),
+    path("cart/", CartView.as_view(), name="cart"),
+    path("add_to_cart/<int:product_id>/", AddToCartView.as_view(), name="add_to_cart"),
     path(
-        'create_stripe_account_link/', create_stripe_account_link,
-        name='create_stripe_account_link'
-        ),
+        "remove_from_cart/<int:product_id>/",
+        RemoveFromCartView.as_view(),
+        name="remove_from_cart",
+    ),
+    path("checkout_success/", CheckoutSuccessView.as_view(), name="checkout_success"),
+    path("checkout/", CheckoutView.as_view(), name="checkout"),
+    path("product-sold/<int:pk>/", ProductSoldView.as_view(), name="product_sold"),
+    path("order-page/<int:pk>/", OrderPageView.as_view(), name="order-page"),
     path(
-        'handle_stripe_return/', handle_stripe_return,
-        name='handle_stripe_return'
-        ),
-    path('about-us/', AboutUsView.as_view(), name='about-us'),
-    path('contact/', ContactUsView.as_view(), name='contact'),
-    path('privacy/', PrivacyView.as_view(), name='privacy'),
-    path('terms/', TermsView.as_view(), name='terms'),
-    path(
-        'reset-password/', ResetPasswordView.as_view(),
-        name='reset-password'
-        ),
-    path(
-        'reset-password/<str:token>/', ResetPasswordConfirmView.as_view(),
-        name='reset-password-confirm'
-        ),
-
-]
+        "order-confirmation/<int:pk>/",
+        OrderConfirmation.as_view(),
+        name="order-confirmation",
+    ),
+    path("search/", SearchView.as_view(), name="search"),
+    path("shop/", ShopView.as_view(), name="shop"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
